c96372807408f4d65524fcd5fb80421f052960b4
Ryan Haig <haigryan@gmail.com>
2019-09-30 21:19:24 -0600

    New League Page details and created button to team page

--- client/src/components/SingleLeaguePage.js
+++ client/src/components/SingleLeaguePage.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-const SingleLeaguePage = ({ match, usersLeagues }) => {
+const SingleLeaguePage = ({ match, usersLeagues, usersTeams }) => {
 
     const [league, setLeague] = useState()
+    const [userTeam, setUserTeam] = useState()
 
     const redirectRoute = `/leagues/${match.params.leagueId}/createTeam`
 
@@ -13,25 +14,51 @@ const SingleLeaguePage = ({ match, usersLeagues }) => {
             setLeague( usersLeagues.filter(leg => leg._id === match.params.leagueId )[0] )
     }, [usersLeagues])
 
+    useEffect(() => {
+        if(usersTeams.length)
+            setUserTeam( usersTeams.filter(team => team.leagueId === match.params.leagueId)[0] )
+    })
+
+    const renderUserTeam = () => {
+        // if user has a team, return jsx of the team name and
+        // a button to the single team page
+        if(userTeam.teamName){
+            //console.log(userTeam)
+            const userTeamRoute = `/leagues/${match.params.leagueId}/teams/${userTeam._id}`
+            return (
+                <div>
+                    <h2>Your Team: { userTeam.teamName }</h2>
+                    <Link to={ userTeamRoute }>
+                        <button>Visit Team Page</button>
+                    </Link>
+                </div>
+            )
+        }
+
+        // if user has no team, render the option to
+        // create a team for the given league
+        return (
+            <div>
+                <h2>No team? Create one now!</h2>
+                <Link to={ redirectRoute }>
+                    <button>Create Team</button>
+                </Link>
+            </div>
+        )
+    }
+
     if(league) {
-        console.log(league)
         return (
             <div>
                 <h3>League Name: { league.leagueName }</h3>
                 <h4>League Type: { league.leagueType }</h4>
                 <h4>League Managers: { league.leagueManagers.map(({ id }) => {
-                    console.log(id)
                     return (
                         <p key={ id }>{ id }</p>
                     )
                 })}</h4>
 
-                <div>
-                    <h2>No team? Create one now!</h2>
-                    <Link to={ redirectRoute }>
-                        <button>Create Team</button>
-                    </Link>
-                </div>
+                { renderUserTeam() }
             </div>
         )
     }
@@ -40,7 +67,8 @@ const SingleLeaguePage = ({ match, usersLeagues }) => {
 
 const mapStateToProps = state => {
     return {
-        usersLeagues: state.Leagues.usersLeagues
+        usersLeagues: state.Leagues.usersLeagues,
+        usersTeams: state.FantasyTeams.usersTeams
     }
 }
 
