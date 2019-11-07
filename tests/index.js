01c678318337b166636d86ee614997b8519f03d0
Michael Dreesen <michaeldreesen@Michaels-MBP.lan1>
2019-11-06 21:07:22 -0700

    dreesens initial selenium commit

--- tests/index.js
+++ tests/index.js
@@ -3,14 +3,20 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async () => {
   let driver = await new Builder().forBrowser('chrome').build()
   try {
-    await driver.get('localhost:3000/register')
+    await driver.get('https://fantasy-basketball-haig-turley.herokuapp.com')
 
     await driver.findElement(By.id('nameInputBox')).click()
-    await driver.getTitle()
-
+    const title = await driver.getTitle()
+	console.log(title)
+  console.log("hello_world")
   } finally {
 
-    await driver.quit()
+    driver.get('https://fantasy-basketball-haig-turley.herokuapp.com');
+    driver.getEelement(By.xpath('/html/body/div/div/div/div/div')).click();
+    deiver.findElement(By.className('sc-iwsKbI gYYcnc')).click();
+    driver.findElement(By.name('name')).sendkeys('testing');
 
   }
 })();
+
+    // await driver.quit()
